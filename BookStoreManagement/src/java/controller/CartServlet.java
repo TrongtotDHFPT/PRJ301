4a69f02064daf70494fcf9416f6a6446d9cd9b0c
NguyenTrongTot <164918193+TrongtotDHFPT@users.noreply.github.com>
2025-03-26 19:35:52 +0700

    update

--- BookStoreManagement/src/java/controller/CartServlet.java
+++ BookStoreManagement/src/java/controller/CartServlet.java
@@ -12,8 +12,6 @@ import dto.ProductDTO;
 import dto.UserDTO;
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
